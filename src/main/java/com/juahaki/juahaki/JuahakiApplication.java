05f285c681bb90c23168da3f57f207e032af4a61
newton <wamitinewton@gmail.com>
2025-06-12 05:25:59 +0300

    init

---
+++ src/main/java/com/juahaki/juahaki/JuahakiApplication.java
@@ -0,0 +1,13 @@
+package com.juahaki.juahaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuahakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuahakiApplication.class, args);
+	}
+
+}
