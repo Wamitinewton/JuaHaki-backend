5e9010ba8a83afa7e9de4f9a719f31af83508b7e
newton <wamitinewton@gmail.com>
2025-06-13 12:38:32 +0300

    project config

--- src/main/java/com/juahaki/juahaki/JuahakiApplication.java
+++ src/main/java/com/juahaki/juahaki/JuahakiApplication.java
@@ -2,6 +2,7 @@ package com.juahaki.juahaki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class JuahakiApplication {
@@ -9,5 +10,6 @@ public class JuahakiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JuahakiApplication.class, args);
 	}
+
 
 }
